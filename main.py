d43645c68e2cf755ac2ec5c1028e4ad49a986a8e
aa443344 <112803182+aa443344@users.noreply.github.com>
2022-09-04 23:21:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def get_weather(region):
     # 天气
     weather = weatherinfo["weather"] 
     # 最高气温
-    temp = weatherinfo["temp"] 
+    weather = weatherinfo["temp"] 
     # 最低气温
     temp = response["now"]["temp"] + u"\N{DEGREE SIGN}" + "C"
     # 风向
